dbe0e86e7f235cfaf1b5017a7bec818fcfb9b552
Aurnab Paul <105810364+aurnabnil66@users.noreply.github.com>
2024-10-09 16:28:03 +0600

    TEAM_PARMA-01: drawer greetings message created for guest and drawe

--- src/navigators/UserDrawerNavigator.tsx
+++ src/navigators/UserDrawerNavigator.tsx
@@ -13,11 +13,13 @@ const Drawer = createDrawerNavigator();
 const getGreeting: any = () => {
   const currentHour = new Date().getHours();
 
-  if (currentHour < 12) {
+  if (currentHour > 0 && currentHour < 12) {
     return 'Good Morning';
-  } else if (currentHour < 18) {
-    return 'Good Afternoon';
-  } else if (currentHour > 18 && currentHour <= 21) {
+  } else if (currentHour > 12 && currentHour < 16) {
+    return 'Good Noon';
+  } else if (currentHour > 16 && currentHour < 18) {
+    return 'Good AfterNoon';
+  } else if (currentHour > 18 && currentHour < 21) {
     return 'Good Evening';
   } else {
     return 'Good Night';
